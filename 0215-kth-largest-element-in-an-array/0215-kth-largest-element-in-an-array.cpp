c598c49b2be1f63171e2aed940120e34540bb2d6
Mohamed Amir <mooamir90@gmail.com>
2024-06-29 14:21:43 +0300

    Time: 87 ms (74.03%), Space: 64.3 MB (22.26%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,67 +1,15 @@
 class Solution {
 public:
-   void merge(vector<int>& arr, int left, int mid, int right) {
-	int n1 = mid - left + 1;
-	int n2 = right - mid;
 
-	// Create temporary arrays
-	vector<int> L(n1);
-	vector<int> R(n2);
-
-	// Copy data to temporary arrays L[] and R[]
-	for (int i = 0; i < n1; i++)
-		L[i] = arr[left + i];
-	for (int j = 0; j < n2; j++)
-		R[j] = arr[mid + 1 + j];
-
-	// Merge the temporary arrays back into arr[left..right]
-
-	int i = 0; 
-	int j = 0; 
-	int k = left; // Initial index of merged subarray
-
-	while (i < n1 && j < n2) {
-		if (L[i] <= R[j]) {
-			arr[k] = L[i];
-			i++;
-		}
-		else {
-			arr[k] = R[j];
-			j++;
-		}
-		k++;
-	}
-
-	// Copy the remaining elements of L[], if there are any
-	while (i < n1) {
-		arr[k] = L[i];
-		i++;
-		k++;
-	}
-
-	// Copy the remaining elements of R[], if there are any
-	while (j < n2) {
-		arr[k] = R[j];
-		j++;
-		k++;
-	}
-}
-
-void mergeSort(vector<int>& arr, int left, int right) {
-	if (left < right) {
-		int mid = left + (right - left) / 2;
-
-		// Sort first and second halves
-		mergeSort(arr, left, mid);
-		mergeSort(arr, mid + 1, right);
-
-		// Merge the sorted halves
-		merge(arr, left, mid, right);
-	}
-}
 
 int findKthLargest(vector<int>& nums,int pos) {
-	mergeSort(nums, 0, nums.size() - 1);
-	return nums[nums.size() - pos];
+	priority_queue<int> que;
+	for (auto x: nums)
+		que.push(x);
+	for (int i = 0; i < pos-1; i++) {
+		que.pop();
+	}
+	return que.top();
+
 }
 };
