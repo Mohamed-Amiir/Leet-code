f9f95a2f3563f6c8551b1a67fa657b1ffe3405ab
Mohamed Amir <mooamir90@gmail.com>
2024-08-16 20:55:04 +0300

    Time: 225 ms (44.45%), Space: 0B (100%) - LeetHub

---
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -0,0 +1,21 @@
+SELECT 
+    machine_id, 
+    ROUND(AVG(duration), 3) AS processing_time
+FROM (
+    SELECT 
+        t1.machine_id,
+        t1.process_id,
+        t1.timestamp AS start_time,
+        t2.timestamp AS end_time,
+        t2.timestamp - t1.timestamp AS duration
+    FROM 
+        Activity t1
+    JOIN 
+        Activity t2
+    ON 
+        t1.machine_id = t2.machine_id 
+        AND t1.process_id = t2.process_id
+        AND t1.activity_type = 'start'
+        AND t2.activity_type = 'end'
+) AS subquery_alias
+GROUP BY machine_id;
