1d20450544766aa276e956ac7be44059f0b6e813
Mohamed Amir <mooamir90@gmail.com>
2024-08-18 19:17:24 +0300

    Time: 881 ms (50.88%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -1,6 +1,6 @@
 SELECT st.student_id, st.student_name,sb.subject_name ,COUNT(ex.subject_name) as attended_exams
 FROM Students st
-JOIN Subjects sb
+CROSS JOIN Subjects sb
 LEFT JOIN Examinations ex
 ON ex.student_id = st.student_id AND ex.subject_name = sb.subject_name
 GROUP BY st.student_id,sb.subject_name
