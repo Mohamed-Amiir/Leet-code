0d47eded7295ea326a9c882deac94830dd806212
Mohamed Amir <mooamir90@gmail.com>
2024-10-27 19:09:38 +0300

    Time: 4 ms (19.28%), Space: 96.3 MB (6.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,41 +1,13 @@
 class Solution {
 public:
    int maxProfit(vector<int>&prices) {
-	map<int, int> dp;
-	int res = 0;
-	int bestStart = -1;
-	for (int i = 0; i < prices.size() - 1; i++)
-	{
-		if (dp[prices[i]] != 0 || prices[i+1] < prices[i])
-		{
-			continue;
-		}
-		if ((prices[i] > bestStart && bestStart != -1))
-		{
-			continue;
-		}
-		for (int j = i + 1; j < prices.size(); j++)
-		{
-			if (prices[j] - prices[i] > res)
-			{
-				res = prices[j] - prices[i];
-				bestStart = prices[i];
-				dp[prices[i]] = res;
-			}
-            else if(prices[j]-prices[i] < 0){
-                   i = j;
-            }
-			
-		}
-		if (dp[prices[i]] == 0)
-		{
-			dp[prices[i]] = -1;
-		}
-	}
-	return res;
-}
+    int n = prices.size();
+	int a = pow(10,4), x = 0;
 
-
-
-
+    for(int i =0 ;i < n;i++){
+      a = min (a,prices[i]);
+      x = max (x,prices[i] - a);
+    }
+    return x;
+   }
 };
