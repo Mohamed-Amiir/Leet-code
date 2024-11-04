6a8880ace34f1bfe6b68467a7ea72115b76a03a2
Mohamed Amir <mooamir90@gmail.com>
2024-11-04 06:56:29 +0200

    Time: 0 ms (100%), Space: 9 MB (52.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
 bool isPalindrome(string s) {
-    for (int i = 0; i < s.size(); i++) {
-        s[i] = tolower(s[i]);
-        if (!isalnum(s[i])) {
-            s.erase(s.begin() + i);
-            i--; // Adjust index after erasing a character
-        }
-    }
     // lets go
-    int i = 0, j = s.size()-1;
-    while (j != -1) {
-        if (s[i] == ' ')
+    int i = 0, j = s.size() - 1;
+    while (i < j) {
+
+        if (!isalnum(s[i])) {
             i++;
-        if (s[j] == ' ')
+            continue;
+        }
+        else
+            s[i] = tolower(s[i]);
+        if (!isalnum(s[j])) {
             j--;
+            continue;
+        }
+        else
+            s[j] = tolower(s[j]);
 
         if (s[i] != s[j])
             return false;
