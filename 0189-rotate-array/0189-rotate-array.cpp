c96905941c76e87f372b237ef2b17f3ea7f55c16
Mohamed Amir <mooamir90@gmail.com>
2024-11-08 18:07:22 +0200

    Time: 2 ms (11.43%), Space: 28.8 MB (5.21%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-void rotate(vector<int>& nums, int k) {
-	int n = nums.size();
-	k = k % n;
-	if (n != 1)
-	{
-		//reverse nums
-		int i = 0, j = nums.size() - 1;
-		while (j > i)
-		{
-			int temp = nums[i];
-			nums[i] = nums[j];
-			nums[j] = temp;
-			i++; j--;
-		}
-
-		//reverse k of nums
-		i = 0, j = k - 1;
-		while (j > i)
-		{
-			int temp = nums[i];
-			nums[i] = nums[j];
-			nums[j] = temp;
-			i++; j--;
-		}
-		//reverse the rest of nums
-		i = k, j = nums.size() - 1;
-		while (j > i)
-		{
-			int temp = nums[i];
-			nums[i] = nums[j];
-			nums[j] = temp;
-			i++; j--;
-		}
-
-	}
-
-}
-
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        k = nums.size() - k;
+        queue<int>q;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            q.push(nums[i]);
+        }
+        while (k--) {
+            q.push(q.front());
+            q.pop();
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nums[i] = q.front();
+            q.pop();
+        }
+    }
 
 };
