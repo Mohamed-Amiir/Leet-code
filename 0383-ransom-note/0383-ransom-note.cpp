4934b320e572471a0f1a6fd1dba234a302817ea9
Mohamed Amir <mooamir90@gmail.com>
2024-11-21 20:17:17 +0200

    Time: 11 ms (32.05%), Space: 11.6 MB (13.37%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int> mCount;
+        for(char c : magazine){
+            mCount[c]++;
+        }
+        for(char c : ransomNote){
+            if(mCount[c] == 0)
+               return 0;
+            mCount[c]--;
+        }
+        return 1;
+    }
+};
