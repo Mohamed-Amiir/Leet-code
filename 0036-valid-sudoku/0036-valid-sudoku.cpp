20da8920f9756a1f2a8ae6ca40b7352fcb7ed137
Mohamed Amir <mooamir90@gmail.com>
2024-11-27 13:50:50 +0200

    Time: 13 ms (11.24%), Space: 25.4 MB (38.89%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<unordered_map<int,int>>> cubes (3, vector<unordered_map<int,int>>(3));
+        for(int i = 0;i < 9; i++){
+            unordered_map<int,int>colCounter,rowCounter;
+            for(int j = 0; j < 9;j++){
+                if(rowCounter[board[i][j]] == 1 || colCounter[board[j][i]] == 1 ||cubes[i/3][j/3][board[i][j]] == 1)
+                    return false;
+                else{
+                    if(board[i][j] != '.') rowCounter[board[i][j]]++;
+                    if(board[j][i] != '.') colCounter[board[j][i]]++;
+                    if(board[i][j] != '.') cubes[i/3][j/3][board[i][j]]++;
+               }
+            }
+        }
+        return true;
+    }
+};
