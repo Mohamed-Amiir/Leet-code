aa09e0e7154fc61c15ee6589fc0a1a3529c95f3d
Mohamed Amir <mooamir90@gmail.com>
2024-12-01 21:03:44 +0200

    Time: 107 ms (5.06%), Space: 8.7 MB (7.38%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,17 +3,10 @@ public:
     int mySqrt(int x) {
         if(x == 0 || x == 1) return x;
         int left = 0,right = x;
-        while (left <= right){
-            int mid = left + (right - left )/2;
-
-            if ( mid <= x/mid && mid+1 > x/(mid+1)) return mid;
-            else if (mid > x/mid ) 
-               right = mid-1;
-            else 
-               left = mid+1;
-
+        for(int i =2;i < 46341;i++){
+            if ( i <= x/i && i+1 > x/(i+1)) return i;
         }
-        return -1;
+        return 1;
     }
 };
 // 0    8  => 4
