e059230d12615f22f3e3181926427b74bc835a20
Mohamed Amir <mooamir90@gmail.com>
2024-12-02 16:10:03 +0200

    Time: 3 ms (69.45%), Space: 9.1 MB (90.67%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -11,10 +11,8 @@ public:
         while(dig > 0){
             for(auto x: vals){
                 if(x.first <= dig){
-                    cout << "NewDig : " << dig << endl;
                     dig -= x.first;
                     res.push_back(x.second);
-                    cout << "ROMAN: "<< x.second<<endl;
                     break;
                 }
             } 
@@ -29,54 +27,18 @@ public:
         vals.push_back({10,'X'});
         vals.push_back({5,'V'});
         vals.push_back({1,'I'});
-       
-        map<char,int> values;
-        values['I'] = 1;
-        values['V'] = 5;
-        values['X'] = 10;
-        values['L'] = 50;
-        values['C'] = 100;
-        values['D'] = 500;
-        values['M'] = 1000;
+
 
         string result = "";
         int temp = 1000;
         while(num != 0){
             int dig = (num / temp)*temp;
-            cout << "dig : " << dig << endl;
             process(vals,result,dig);
             num = num % temp;
             temp = temp /10;
         }
 
-        // filter result from 4 and 9
-
-        // int c = 0;
-        // string output = "";
-        // string t = "";
-        // t.push_back(result[0]);
-        // for(int i=1;i < result.size();i++)
-        // {
-        //     if(result[i] != result[i-1]){
-        //         if(c==5){
-        //             if(t[0] == 'I'){
-
-        //             }
-        //         }
-        //         else if(c==10)
-        //         else output = output + t;
-        //         t = "";c=0;
-        //     }
-        //     c++;
-        //     t.push_back(result[i])
-        //     if(i == result.size()-1){
-        //         if(c==5)
-        //         else if(c==10)
-        //         else output = output + t;
-        //         t = "";c=0;
-        //     }
-        // }
-        // return output;
+        
         return result;
     }
 };
