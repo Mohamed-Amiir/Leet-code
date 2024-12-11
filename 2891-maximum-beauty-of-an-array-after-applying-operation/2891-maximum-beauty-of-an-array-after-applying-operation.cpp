bc9d6a04a508960f50694985f4d028d295e19eba
Mohamed Amir <mooamir90@gmail.com>
2024-12-11 19:35:07 +0200

    Time: 190 ms (34.44%), Space: 140.5 MB (33.59%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -12,19 +12,15 @@ public:
            pair<int,int> range = operation(nums[i],k);
            points.push_back({range.first,true});
            points.push_back({range.second+1,false});
-
         }
+        
         sort(points.begin(),points.end());
         int result = 0,c=0;
         for(auto p : points){
-            if(p.second){
-                c++;
-            }
-            else{
-                c--;
-            }
+            p.second?c++:c--;
             result = max(result,c);
         }
         return result;
     }
 };
+
