e12330672b63bfad5ebde3546ca7a88b347b18ac
Mohamed Amir <mooamir90@gmail.com>
2024-12-31 00:48:11 +0200

    Time: 4 ms (78.17%), Space: 11.7 MB (62.57%) - LeetHub

---
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    //to be rechecked
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        const int MOD = 1e9 + 7;
+        vector<int> dp(high + 1, 0);
+        dp[0] = 1; // Base case: There's one way to construct a string of length 0.
+
+        for (int i = 0; i <= high; ++i) {
+            if (dp[i] > 0) {
+                if (i + zero <= high) {
+                    dp[i + zero] = (dp[i + zero] + dp[i]) % MOD;
+                }
+                if (i + one <= high) {
+                    dp[i + one] = (dp[i + one] + dp[i]) % MOD;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = low; i <= high; ++i) {
+            result = (result + dp[i]) % MOD;
+        }
+
+        return result;
+    }
+};
