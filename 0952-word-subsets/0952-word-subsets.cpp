04f8f2fa5978fabaa7cbf6096219ad4c7cb69235
Mohamed Amir <mooamir90@gmail.com>
2025-01-10 18:43:01 +0200

    Time: 48 ms (67.58%), Space: 84.5 MB (72.15%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.cpp
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        unordered_map<char,int>counter,temp;
+        vector<int> counter(26,0);
+        // unordered_map<char,int>counter,temp;
         for(auto s: words2){
+            vector<int> temp(26,0);
             for(auto c : s){
-                temp[c]++;
-                counter[c] = max(counter[c],temp[c]);
+                temp[c-'a']++;
+                counter[c-'a'] = max(counter[c-'a'],temp[c-'a']);
             }
-            temp.clear();
         }
 
         vector<string> result;
         // bool flag = 1;
         for(auto s:words1){
             bool flag = 1;
-           for(auto x : counter){
-             if(x.second > count(s.begin(),s.end(),x.first)){
+           for(int i=0;i<26 ;i++){
+             if(counter[i] > count(s.begin(),s.end(),'a'+i)){
                 flag = 0;
                 break;
              }
