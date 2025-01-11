95216027a8a4b1b1a50879e86dc2d6c87c9cf6a3
Mohamed Amir <mooamir90@gmail.com>
2025-01-11 21:18:39 +0200

    Time: 0 ms (100%), Space: 14.8 MB (66.78%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool canConstruct(string s, int k) {
         if(s.size() < k) return false;
-        unordered_map<char,int> chars;
+        int chars[26] = {};
         int c = 0;
-        for(auto c:s){
-           chars[c]++;
+        for(auto x : s){
+           chars[x-'a']++;
         }
         for(auto x : chars){
-            if(x.second % 2 != 0)c++;
+            if(x % 2 != 0)c++;
         }
         return c <= k;
     }
