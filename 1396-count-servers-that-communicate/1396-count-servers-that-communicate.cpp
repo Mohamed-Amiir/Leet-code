9da66b6835b6f6c8f12f6323ee95bd8ccc6cc758
Mohamed Amir <mooamir90@gmail.com>
2025-01-23 22:08:31 +0200

    Time: 0 ms (100%), Space: 26.7 MB (59.54%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -2,33 +2,15 @@ class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
         int result = 0, m = grid.size(), n = grid[0].size();
-        unordered_map<int,int> rows,columns;
-        // unordered_map<int,vector<int>> rowsOfColumns;
+        vector<int> rows(m),columns(n);
         for(int i = 0; i < m ;i++){
             for(int j = 0; j < n ; j++){
                if(grid[i][j] == 1){
                 rows[i]++;
                 columns[j]++;
-            //     if(rows[i] > 1)
-            //        rowsOfColumns[j].push_back(i);
                }
             }
         }
-
-        // for(auto r : rows){
-        //     if(r.second > 1)
-        //        result += r.second;
-        // }
-        // for(auto c : columns){
-        //     if(c.second > 1)
-        //        result += c.second;
-        // }
-        // for(auto c : columns){
-        //     for(int rc : rowsOfColumns[c.first]){
-        //         if(rows[rc] > 1)result--;
-        //         else if(c.second > 1) result++;
-        //     }
-        // }
         for(int i = 0; i < m ;i++){
             for(int j = 0; j < n ; j++){
                if(grid[i][j] == 1){
