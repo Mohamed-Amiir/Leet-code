0e1f2630958891f5c479a77d938eb85cd9cf3f88
Mohamed Amir <mooamir90@gmail.com>
2025-01-23 22:06:43 +0200

    Time: 8 ms (30.67%), Space: 26.4 MB (82.47%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -3,26 +3,26 @@ public:
     int countServers(vector<vector<int>>& grid) {
         int result = 0, m = grid.size(), n = grid[0].size();
         unordered_map<int,int> rows,columns;
-        unordered_map<int,vector<int>> rowsOfColumns;
+        // unordered_map<int,vector<int>> rowsOfColumns;
         for(int i = 0; i < m ;i++){
             for(int j = 0; j < n ; j++){
                if(grid[i][j] == 1){
                 rows[i]++;
                 columns[j]++;
-                if(rows[i] > 1)
-                   rowsOfColumns[j].push_back(i);
+            //     if(rows[i] > 1)
+            //        rowsOfColumns[j].push_back(i);
                }
             }
         }
 
-        for(auto r : rows){
-            if(r.second > 1)
-               result += r.second;
-        }
-        for(auto c : columns){
-            if(c.second > 1)
-               result += c.second;
-        }
+        // for(auto r : rows){
+        //     if(r.second > 1)
+        //        result += r.second;
+        // }
+        // for(auto c : columns){
+        //     if(c.second > 1)
+        //        result += c.second;
+        // }
         // for(auto c : columns){
         //     for(int rc : rowsOfColumns[c.first]){
         //         if(rows[rc] > 1)result--;
@@ -32,7 +32,7 @@ public:
         for(int i = 0; i < m ;i++){
             for(int j = 0; j < n ; j++){
                if(grid[i][j] == 1){
-                if(rows[i] > 1 && columns[j] > 1)result--;
+                if(rows[i] > 1 || columns[j] > 1)result++;
                }
             }
         }
