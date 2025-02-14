8bd3c55bab21b7f7fd9614ee5d5b844cf036e6d3
Mohamed Amir <mooamir90@gmail.com>
2025-02-14 21:00:12 +0200

    Time: 25 ms (47.36%), Space: 78 MB (77.84%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,20 +1,34 @@
 class ProductOfNumbers {
 public:
-    vector<int> nums;
+    vector<int> nums, pp;
+    
     ProductOfNumbers() {
-        
+        pp.push_back(1);
     }
-    
+
     void add(int num) {
-        nums.push_back(num);
+        
+            if(num == 0){
+                pp.clear();
+               pp.push_back(1);
+            }
+            else if(pp.size() == 0){
+                pp.push_back(num);
+            }
+            else
+               pp.push_back(num * pp.back());
     }
-    
+
     int getProduct(int k) {
-        int result=1;
-        for(int i = nums.size()-k;i<nums.size();i++){
-            result *= nums[i];
-        }
-        return result;
+        if (pp.size() <= k)
+            return 0;
+        return pp.back() / pp[pp.size() - k - 1];
+
+        // int result=1;
+        // for(int i = nums.size()-k;i<nums.size();i++){
+        //     result *= nums[i];
+        // }
+        // return result;
     }
 };
 
