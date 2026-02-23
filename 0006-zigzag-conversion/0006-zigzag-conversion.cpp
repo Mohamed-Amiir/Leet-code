c55d79d4eed2af98e0777c9e4dfd4d4ad64c29d1
Mohamed Amir <mooamir90@gmail.com>
2026-02-23 13:50:24 +0200

    Time: 70 ms (5.06%), Space: 43 MB (5.02%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1)return s;
+        int numCols = s.size();
+        vector<vector<char>> matrix(numRows, vector<char>(numCols));
+        // char matrix[numRows][numCols];
+        int r = 0, c = 0;
+        bool flag = true;
+        for (char a : s) {
+
+            matrix[r][c] = a;
+
+            if (flag)
+                r++;
+            else {
+                r--;
+                c++;
+            }
+
+            if (r == numRows - 1) {
+                flag = false;
+            }
+            if (r == 0) {
+                flag = true;
+            }
+        }
+        string result = "";
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (matrix[i][j] != '\0')
+                    result += matrix[i][j];
+            }
+        }
+        return result;
+    }
+};
