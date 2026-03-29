298b2111b0a0943748cc944e918ed024289736ec
Mohamed Amir <mooamir90@gmail.com>
2026-03-29 17:43:34 +0200

    Time: 0 ms (100%), Space: 8.4 MB (48.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,14 +5,8 @@ public:
     int climbStairs(int n) {
         if (n == 0 || n == 1)
             return 1;
-        int x = 0, y = 0;
-        if (dp[n - 1] == 0)
-            x = climbStairs(n - 1);
-        else x = dp[n - 1];
-        if (dp[n - 2] == 0)
-            y = climbStairs(n - 2);
-        else y = dp[n - 2];
-        dp[n] = x+y;
-        return x + y;
+        if (dp.find(n) != dp.end())
+            return dp[n];
+        return dp[n] = climbStairs(n-1)+climbStairs(n-2);
     }
 };
