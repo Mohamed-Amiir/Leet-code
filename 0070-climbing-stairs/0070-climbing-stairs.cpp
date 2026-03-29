8b7e51391c6868d4e6adf33262041836968de64b
Mohamed Amir <mooamir90@gmail.com>
2026-03-29 17:41:23 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    unordered_map<int, int> dp;
+
+    int climbStairs(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        int x = 0, y = 0;
+        if (dp[n - 1] == 0)
+            x = climbStairs(n - 1);
+        else x = dp[n - 1];
+        if (dp[n - 2] == 0)
+            y = climbStairs(n - 2);
+        else y = dp[n - 2];
+        return x + y;
+    }
+};
