4f4a8413aaf0769d1649c5de7f80b1f5a6dd26a9
Mohamed Amir <mooamir90@gmail.com>
2026-04-14 15:20:53 +0200

    Time: 350 ms (65.72%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,17 +1,17 @@
-WITH latest_dates AS (
-    SELECT product_id, MAX(change_date) AS max_date
+    WITH latest_dates AS (
+        SELECT product_id, MAX(change_date) AS max_date
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY product_id
+    )
+
+
+    SELECT product_id , new_price AS price
     FROM Products
-    WHERE change_date <= '2019-08-16'
-    GROUP BY product_id
-)
+    WHERE (product_id,change_date) IN (SELECT product_id,max_date FROM latest_dates)
 
+    UNION ALL
 
-SELECT product_id , new_price AS price
-FROM Products
-WHERE (product_id,change_date) IN (SELECT product_id,max_date FROM latest_dates)
-
-UNION 
-
-SELECT DISTINCT product_id , 10
-FROM Products
-WHERE product_id NOT IN (SELECT product_id FROM latest_dates)
+    SELECT DISTINCT product_id , 10
+    FROM Products
+    WHERE product_id NOT IN (SELECT product_id FROM latest_dates)
