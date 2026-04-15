8ae9903cc49435b6f7fdace12f6b86c3fe0ebf28
Mohamed Amir <mooamir90@gmail.com>
2026-04-15 15:18:16 +0200

    Time: 0 ms (100%), Space: 13 MB (6.01%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int s = boxes.size();
-        vector<int> result;
-        for (int i = 0; i < s; i++) {
-            int r = 0;
-            for (int j = 0; j < s; j++) {
-                if(boxes[j] == '1')
-                   r += abs(i-j);
-            }
-            result.push_back(r);
+        int n  = boxes.size();
+        vector<int> pr1(n),pr2(n);
+        int c = 0;bool flag = 0;
+
+
+        int oneCounter = 0;
+        if(boxes[0]== '1')oneCounter++;
+
+        pr1[0] = 0;
+        for(int i = 1; i < n; i++){
+            pr1[i] = pr1[i-1] + oneCounter;
+            if(boxes[i] == '1')oneCounter++;
+        }
+
+        oneCounter = 0;
+        if(boxes[n-1]== '1')oneCounter++;
+        pr2[n-1] = 0;
+
+        for(int i = n-2; i >= 0; i--){
+           pr2[i] = pr2[i+1] + oneCounter;
+           if(boxes[i] == '1')oneCounter++;
+        }
+
+        vector<int>result;
+        for(int i = 0; i < n;i++){
+            result.push_back(pr1[i]+pr2[i]);
         }
         return result;
     }
