881006baeb2f3c9c219547c0f8e9c35bf0e28ed8
Mohamed Amir <mooamir90@gmail.com>
2026-06-11 14:33:26 +0300

    Time: 0 ms (100%), Space: 9.8 MB (25.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s ==t;
+        if(s.size() != t.size()) return 0;
+        int a = 0 ,b = 0;
+        vector<int> freq1(26),freq2(26);
+        for(int i= 0 ; i < s.size();i++){
+            freq1[s[i]-'a']++;
+            freq2[t[i]-'a']++;
+        }
+        for(int i= 0 ; i < 26;i++){
+           if (freq1[i] != freq2[i])return 0;
+        }
+        return 1;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s ==t;
     }
 };
