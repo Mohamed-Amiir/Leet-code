a0971c3f4112105815a56dc24c654e2c0cea287f
Mohamed Amir <mooamir90@gmail.com>
2026-06-12 20:51:40 +0300

    Time: 2 ms (91.28%), Space: 23.4 MB (64.64%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size() != word2.size())return false;
-        set<char> s1(word1.begin(),word1.end());
-        set<char> s2(word2.begin(),word2.end());
-        if(s1 != s2) return false;
-        unordered_map<char,int>freq1,freq2;
-        for(int i  =0 ;i < word1.size();i++){
-            freq1[word1[i]]++;
-            freq2[word2[i]]++;
+        if (word1.size() != word2.size())
+            return false;
+        // set<char> s1(word1.begin(), word1.end());
+        // set<char> s2(word2.begin(), word2.end());
+        // if (s1 != s2)
+        //     return false;
+        // unordered_map<char, int> freq1, freq2;
+        vector<int> freq1(26),freq2(26);
+        for (int i = 0; i < word1.size(); i++) {
+            freq1[word1[i] - 'a']++;
+            freq2[word2[i] - 'a']++;
         }
-        string pattern1 = "", pattern2 = "";
-        for(auto p : freq1){
-            pattern1 += ('0'+p.second);
+        for(int i = 0 ;i < 26 ;i++){
+            if((freq1[i] == 0) != (freq2[i] == 0))
+                return false;
         }
-        for(auto p : freq2){
-            pattern2 += ('0'+p.second);
-        }
-        sort(pattern1.begin(),pattern1.end());
-        sort(pattern2.begin(),pattern2.end());
-        return pattern1 == pattern2;
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
+        return freq1 == freq2;
     }
 };
