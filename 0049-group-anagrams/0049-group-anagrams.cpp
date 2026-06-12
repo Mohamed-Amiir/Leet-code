32a48bdf8bc92bd4264e32844afd57b8de851d9c
Mohamed Amir <mooamir90@gmail.com>
2026-06-12 15:59:55 +0300

    Time: 18 ms (49.92%), Space: 26.2 MB (29.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,20 +3,26 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
         vector<int> signture(26);
-        unordered_map<string, vector<string>> group;
-
-        for (string s : strs) {
-            vector<int> curSign = signture;
-            for (char c : s) {
-                curSign[c - 'a']++;
-            }
-            string strSign = "";
-            for(int x : curSign){
-                strSign += "#" + to_string(x);
-            }
-            group[strSign].push_back(s);
-        }
         vector<vector<string>> result;
+        unordered_map<string, vector<string>> group;
+         
+        for(string s : strs){
+            string k = s;
+            sort(k.begin(),k.end());
+            group[k].push_back(s); 
+        }
+        
+        // for (string s : strs) {
+        //     vector<int> curSign = signture;
+        //     for (char c : s) {
+        //         curSign[c - 'a']++;
+        //     }
+        //     string strSign = "";
+        //     for(int x : curSign){
+        //         strSign += "#" + to_string(x);
+        //     }
+        //     group[strSign].push_back(s);
+        // }
         for (auto p : group) {
             result.push_back(p.second);
         }
