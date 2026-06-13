66e009712b29135098e345ecc197ebd06b629960
Mohamed Amir <mooamir90@gmail.com>
2026-06-13 21:11:42 +0300

    Time: 0 ms (100%), Space: 9.1 MB (8.69%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        stringstream ss(s);
-        string word;
-        while(ss>>word){
+        // stringstream ss(s);
+        // string word;
+        // while(ss>>word){
 
+        // }
+        // return word.size();
+
+        int r = s.size()-1,c = 0;
+        while(s[r] == ' ')r--;
+        while(r >= 0 && s[r] != ' '){
+           c++;
+           r--;
         }
-        return word.size();
+        return c;
     }
 };
