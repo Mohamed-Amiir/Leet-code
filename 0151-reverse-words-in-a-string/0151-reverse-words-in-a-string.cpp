a48bcaf13ed3788bdb658b6fb3df5da1c7db94e8
Mohamed Amir <mooamir90@gmail.com>
2026-06-13 21:27:30 +0300

    Time: 1 ms (46.39%), Space: 10.6 MB (42.92%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,11 +4,31 @@ public:
         stack<string> rev;
         stringstream ss(s);
         string word;
-        while(ss>>word){
-            rev.push(word);
-        }
         string result = "";
-        while(!rev.empty()){
+        // while(ss>>word){
+        //     rev.push(word);
+        // }
+        // while(!rev.empty()){
+        //     result += (rev.top() + ' ');
+        //     rev.pop();
+        // }
+        // result.pop_back();
+        // return result;
+
+        int l = 0;
+        while (l < s.size()) {
+            if (s[l] == ' ') {
+                l++;
+            } else {
+                string word = "";
+                while ( l < s.size() && s[l] != ' ') {
+                    word.push_back(s[l]);
+                    l++;
+                }
+                rev.push(word);
+            }
+        }
+        while (!rev.empty()) {
             result += (rev.top() + ' ');
             rev.pop();
         }
