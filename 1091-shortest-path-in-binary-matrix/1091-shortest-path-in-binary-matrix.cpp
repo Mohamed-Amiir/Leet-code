7396465ab42744a90b2a06231a4936197b7245cf
Mohamed Amir <mooamir90@gmail.com>
2026-06-19 18:55:57 +0300

    Time: 83 ms (12.13%), Space: 44.1 MB (8.79%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -5,21 +5,21 @@ public:
         if (grid[0][0] || grid[n - 1][n - 1])
             return -1;
         if(n == 1)return 1;
-        set<pair<int, int>> visited;
+        vector<vector<int>> visited(n,vector<int>(n,0));
         vector<pair<int, int>> dirs = {{0, 1},  {1, 0},  {0, -1},  {-1, 0},
                                        {1, -1}, {-1, 1}, {-1, -1}, {1, 1}};
         queue<vector<int>> q;
         q.push({0, 0, 1});
-        visited.insert({0, 0});
+        visited[0][0] = 1;
         while (!q.empty()) {
             vector<int> temp = q.front();
             q.pop();
             for (auto d : dirs) {
                 int x = temp[0] + d.first, y = temp[1] + d.second;
                 if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 0 &&
-                    visited.count({x, y}) == 0) {
+                    visited[x][y] == 0) {
                     int newDist = temp[2] + 1;
-                    visited.insert({x, y});
+                    visited[x][y] = 1;
                     q.push({x, y, newDist});
                     if (x == n - 1 && y == n - 1)
                         return newDist;
