b8c13df6aeb3efc9a41283bedb87f34b4e23d211
Mohamed Amir <mooamir90@gmail.com>
2026-06-22 06:21:00 +0300

    Time: 0 ms (100%), Space: 9.2 MB (15.37%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         string target = "balloon";
-        unordered_map<char,int>freq,minFreq;
-        for(char c : target){
-            minFreq[c]++;
-        }
-        for(char c : text){
-           freq[c]++;
-        }
         int result = 1e9;
-        for(char c : target){
-            result = min(result,freq[c] / minFreq[c]);
-        }
-        return result;
+        unordered_map<char, int> freq, minFreq;
 
+        // for(char c : target){
+        //     minFreq[c]++;
+        // }
+        for (char c : text) {
+            freq[c]++;
+        }
+        // for(char c : target){
+        //     result = min(result,freq[c] / minFreq[c]);
+        // }
+
+        return min({freq['b'], freq['a'], freq['l'] / 2, freq['o'] / 2,
+                   freq['n']});
     }
 };
