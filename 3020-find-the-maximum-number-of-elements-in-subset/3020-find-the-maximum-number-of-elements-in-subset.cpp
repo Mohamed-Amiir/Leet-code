1d46edb9030da76a2441ce82b8257084a46294f5
Mohamed Amir <mooamir90@gmail.com>
2026-06-27 16:56:17 +0300

    Time: 75 ms (74.86%), Space: 122.9 MB (25.71%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        unordered_map<long long, int> cnt;
+        unordered_map<long long, int> freq;
         for (int num : nums) {
-            cnt[num]++;
+            freq[num]++;
         }
-        int ans = 0;
-        // ans is at least the number of occurrences of 1, rounded down to an
-        // odd number
-        if (cnt[1] % 2 == 0) {
-            ans = cnt[1] - 1;
+        int result = 0;
+        if (freq[1] % 2 == 0) {
+            result = freq[1] - 1;
         } else {
-            ans = cnt[1];
+            result = freq[1];
         }
-        cnt.erase(1);
-        for (auto p : cnt) {
-            int res = 0;
+        freq.erase(1);
+        for (auto p : freq) {
+            int temp = 0;
             long long x = p.first;
-            for (; cnt.contains(x) && cnt[x] > 1; x *= x) {
-                res += 2;
+            for (; freq.contains(x) && freq[x] > 1; x *= x) {
+                temp += 2;
             }
-            ans = max(ans, res + (cnt.contains(x) ? 1 : -1));
+            result = max(result, temp + (freq.contains(x) ? 1 : -1));
         }
-        return ans;
+        return result;
     }
 };
