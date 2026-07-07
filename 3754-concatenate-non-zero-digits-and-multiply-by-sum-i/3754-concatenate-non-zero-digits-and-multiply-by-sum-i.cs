0684e741cd82c76ea72b7c6bd458e3cd9651f24e
Mohamed Amir <mooamir90@gmail.com>
2026-07-07 18:25:57 +0300

    Time: 0 ms (100%), Space: 29.1 MB (75%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cs
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cs
@@ -2,21 +2,19 @@ public class Solution {
     public long SumAndMultiply(int n) {
         long sum = 0;
         long newInt = 0;
-        Stack<int> s = new();
+        int place = 1;
+
         while(n > 0){
 
             int digit = n % 10;
             n = n / 10;
             sum += digit;
             if(digit != 0){
-                s.Push(digit);
+                newInt += (place * digit);
+                place = place * 10;
             }
         }
 
-        while(s.Count > 0){
-            int t = s.Pop();
-            newInt = newInt * 10 + t;
-        }
 
         return  newInt * sum;
     }
